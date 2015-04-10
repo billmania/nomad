cfacc4e36b2590a02c3c2fb528135b2dcd46dcdc
Bill Mania <bill@manialabs.us>
2015-04-10 10:49:22 -0700

    Optimized the obstacle detection to the sides, to respond with more precision.

--- nomad/nodes/obstacle_detect_and_avoid.py
+++ nomad/nodes/obstacle_detect_and_avoid.py
@@ -17,7 +17,6 @@ import random
 import roslib; roslib.load_manifest('nomad')
 import rospy
 
-from std_msgs.msg import Bool
 from sensor_msgs.msg import LaserScan
 
 from geometry_msgs.msg import Twist
@@ -30,17 +29,21 @@ class CollisionDetector():
         self.avoidTwistMessage = Twist()
 
         self.avoidTwistMessage.linear.x = 0.0
-        self.detection_threshold = 0.7
-        self.backup_speed = 0.3
-        self.turn_speed = 0.5
-        self.in_front = False
-        self.toss = 0
+        self.avoidTwistMessage.angular.z = 0.0
+        self.minimum_range_to_obstacle = 0.7 # meters
+        self.backup_speed = 0.3 # meters per second
+        self.turn_speed = 0.5 # radians per second
+        self.already_turning = False
+        self.turn_direction = random.choice([-1, 1])
+        #
+        # after the ranges have been sorted into ascending order, which
+        # is the first one to actually examine.
+        #
+        self.range_to_examine = 2
+        rospy.loginfo("Initializing collision_detector node with obstacle distance threshold %0.1f" % (self.minimum_range_to_obstacle))
 
-        rospy.loginfo("Initializing collision_detector node with distance threshold %0.1f" % (self.detection_threshold))
-
-        self.obstacle_detected = Bool()
-
-        self.obstacle_detected.data = True
+        self.filtered_cmd_vel1 = rospy.Publisher('filtered_cmd_vel',Twist, queue_size = 10)
+        self.filtered_cmd_vel1.publish(self.twistMessage)
 
         # get most recent twist on cmd_vel
         self.cmd_vel1 = rospy.Subscriber('cmd_vel', Twist, self.handleTwistMessage)
@@ -48,13 +51,25 @@ class CollisionDetector():
         # callback on laser
         self.callback = rospy.Subscriber('scan', LaserScan, self.laser_callback)
 
-        self.filtered_cmd_vel1 = rospy.Publisher('filtered_cmd_vel',Twist, queue_size = 10)
+    def turnDirection(self):
+        """turnDirection() - determine which way to turn
 
-        self.filtered_cmd_vel1.publish(self.twistMessage)
+        If the rover isn't already turning, randomly choose a direction. Otherwise,
+        continue to turn in the same direction.
+        """
 
-        self.obstaclePublisher = rospy.Publisher('obstacle', Bool, queue_size = 10)
+        if not self.already_turning:
+            self.already_turning = True
+            self.turn_direction = random.choice([-1, 1])
+            rospy.logdebug('Setting turn direction to %d' % self.turn_direction)
+
+        return self.turn_direction
 
     def handleTwistMessage(self,twistMessage):
+        """handleTwistMessage() - Just save the most recent Twist message
+
+        """
+
         rospy.logdebug("translationX: %f, rotationZ: %f" % (
             twistMessage.linear.x,
             twistMessage.angular.z
@@ -70,54 +85,76 @@ class CollisionDetector():
             self.filtered_cmd_vel1.publish(self.twistMessage)
             return
 
-        # Post 'avoid' twist message if object is within 1 meter
-        # in the specified range.
-        # The range values given are attempting to go from -45
-        # -> 45 field of view and assume the scan values range from
-        # 0 -> 180.
+        #
+        # Divide the scan data into three sections: the left quarter, the middle half
+        # and the right quarter. This logic assumes the LiDAR has a Field of View of
+        # 180 degrees.
+        #
+        end_of_left_side = len(msg.ranges)/4
+        begin_of_right_side = 3*len(msg.ranges)/4
 
-        min_ind = len(msg.ranges)/4
-        max_ind = 3*len(msg.ranges)/4
+        left_side = msg.ranges[0:end_of_left_side]
+        right_side = msg.ranges[begin_of_right_side:]
+        front = msg.ranges[end_of_left_side:begin_of_right_side]
+        left_side = sorted(left_side)
+        right_side = sorted(right_side)
+        front = sorted(front)
 
-        backup = 0
+        something_on_the_left = left_side[self.range_to_examine] < self.minimum_range_to_obstacle
+        something_on_the_right = right_side[self.range_to_examine] < self.minimum_range_to_obstacle
 
-        for scan in msg.ranges[0:min_ind]:
-            if (scan < self.detection_threshold):
-                backup = 1
+        if front[self.range_to_examine] < self.minimum_range_to_obstacle:
+            # something is directly in front of the rover
+            rospy.logdebug('Something in front')
+            self.avoidTwistMessage.linear.x = 0.0
 
-        for scan in msg.ranges[max_ind:]:
-            if (scan < self.detection_threshold):
-                backup = 1
-
-        for scan in msg.ranges[min_ind:max_ind]:
-            if (scan < self.detection_threshold):
-                self.obstacle_detected.data = True
-                self.obstaclePublisher.publish(self.obstacle_detected)
-
-                # Randomly choose a direction of angular movement
-                if not self.in_front:
-                    self.toss=random.randint(0,1)
-
-                self.in_front = True
-
-                self.avoidTwistMessage.linear.x = -1.0*self.backup_speed*backup
-
-                if self.toss == 0:
-                    self.avoidTwistMessage.angular.z = self.turn_speed
-                else:
-                    self.avoidTwistMessage.angular.z = -(self.turn_speed)
-
+            if something_on_the_left and something_on_the_right:
+                rospy.logdebug(' and on both sides')
+                self.avoidTwistMessage.linear.x = -1 * self.backup_speed
+                self.avoidTwistMessage.angular.z = self.turn_speed * self.turnDirection()
                 self.filtered_cmd_vel1.publish(self.avoidTwistMessage)
 
-                return
+            elif something_on_the_left and not something_on_the_right:
+                rospy.logdebug(' and on the left')
+                self.avoidTwistMessage.angular.z = self.turn_speed
+                self.filtered_cmd_vel1.publish(self.avoidTwistMessage)
 
-        self.in_front = False
-        self.filtered_cmd_vel1.publish(self.twistMessage)
+            elif not something_on_the_left and something_on_the_right:
+                rospy.logdebug(' and on the right')
+                self.avoidTwistMessage.angular.z = -(self.turn_speed)
+                self.filtered_cmd_vel1.publish(self.avoidTwistMessage)
 
+            else:
+                self.avoidTwistMessage.angular.z = self.turn_speed * self.turnDirection()
+                self.filtered_cmd_vel1.publish(self.avoidTwistMessage)
+
+        else:
+            if front[self.range_to_examine] < (self.minimum_range_to_obstacle * 1.05):
+                # the rover is approaching something in the distance
+                rospy.logdebug('Something approaching')
+                self.twistMessage.linear.x = self.twistMessage.linear.x / 2.0
+                self.filtered_cmd_vel1.publish(self.twistMessage)
+
+            if something_on_the_left and not something_on_the_right:
+                rospy.logdebug(' Something on the left')
+                self.twistMessage.angular.z = self.twistMessage.angular.z + (self.turn_speed / 4)
+                self.filtered_cmd_vel1.publish(self.twistMessage)
+
+            elif not something_on_the_left and something_on_the_right:
+                rospy.logdebug(' Something on the right')
+                self.twistMessage.angular.z = self.twistMessage.angular.z - (self.turn_speed / 4)
+                self.filtered_cmd_vel1.publish(self.twistMessage)
+
+            else:
+                rospy.logdebug('Clear sailing!')
+                self.already_turning = False
+                self.filtered_cmd_vel1.publish(self.twistMessage)
+
+        return
 
 if __name__ == '__main__':
 
-    rospy.init_node('obstacle_detect_and_avoid')
+    rospy.init_node('obstacle_detect_and_avoid', log_level = rospy.DEBUG)
     collision_detector = CollisionDetector()
     rospy.spin()
 
