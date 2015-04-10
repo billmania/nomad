28b792999cc94c200da9c3d4db94e396abae8045
Bill Mania <bill@manialabs.us>
2015-04-09 17:17:07 -0700

    DOES NOT WORK CORRECTLY.
    When an obstacle is detected in front, the wheels oscillate, instead
    of steadily turning or steadily reversing.

--- nomad/nodes/obstacle_detect_and_avoid.py
+++ nomad/nodes/obstacle_detect_and_avoid.py
@@ -27,37 +27,40 @@ class CollisionDetector():
 
     def __init__(self):
 
-	
-	self.twistMessage =  Twist()
-	self.avoidTwistMessage = Twist()
 
-	self.avoidTwistMessage.linear.x = 0.0
-	self.detection_threshold = 0.5
-	self.backup_speed = 0.6
+        self.twistMessage =  Twist()
+        self.avoidTwistMessage = Twist()
+
+        self.avoidTwistMessage.linear.x = 0.0
+        self.detection_threshold = 0.5
+        self.backup_speed = 0.6
+
+        self.in_front = False
+        self.toss = 0
 
         rospy.loginfo("Initializing collision_detector node with distance threshold %0.1f" % (self.detection_threshold))
 
         self.obstacle_detected = Bool()
 
-	self.obstacle_detected.data = True
+        self.obstacle_detected.data = True
 
-	# get most recent twist on cmd_vel
-	self.cmd_vel1 = rospy.Subscriber('cmd_vel', Twist, self.handleTwistMessage)
-	
-	# callback on laser
+        # get most recent twist on cmd_vel
+        self.cmd_vel1 = rospy.Subscriber('cmd_vel', Twist, self.handleTwistMessage)
+    
+        # callback on laser
         self.callback = rospy.Subscriber('scan', LaserScan, self.laser_callback)
 
-	   # check for obstacle
+       # check for obstacle
 #        self.callback = rospy.Subscriber('obstacle', LaserScan, self.laser_callback)
 
-	   #if none publish cmd_vel to filtered_cmd_vel	
+       #if none publish cmd_vel to filtered_cmd_vel    
 
-        self.filtered_cmd_vel1 = rospy.Publisher('filtered_cmd_vel',Twist, queue_size = 10) 	
+        self.filtered_cmd_vel1 = rospy.Publisher('filtered_cmd_vel', Twist, queue_size = 10)     
 
-	self.filtered_cmd_vel1.publish(self.twistMessage)
+        self.filtered_cmd_vel1.publish(self.twistMessage)
 
-	   #else publish obstacle
-	   #publish turn/twist
+       #else publish obstacle
+       #publish turn/twist
 
         self.obstaclePublisher = rospy.Publisher('obstacle', Bool, queue_size = 10)
 
@@ -69,66 +72,76 @@ class CollisionDetector():
         rospy.logdebug("translationX: %f, rotationZ: %f" % (
             twistMessage.linear.x,
             twistMessage.angular.z
-            ))
+        ))
 
-	self.twistMessage = twistMessage
-#	self.filtered_cmd_vel1.publish(self.twistMessage)
-		
-    	return
+        self.twistMessage = twistMessage
+        self.filtered_cmd_vel1.publish(self.twistMessage)            
+
+        return
 
     def laser_callback(self, msg):
 
-	if self.twistMessage.linear.x == 0 and self.twistMessage.angular.y == 0:
- 	    self.filtered_cmd_vel1.publish(self.twistMessage)
-	    return
+        if self.twistMessage.linear.x == 0 and self.twistMessage.angular.y == 0:
+            self.filtered_cmd_vel1.publish(self.twistMessage)
+            return
 
-        min_scan_angle = msg.angle_min
-        max_scan_angle = msg.angle_max
-        num_scan_pts = len(msg.ranges)	
-	scan_angle_incr = msg.angle_increment
+        # Post 'avoid' twist message if object is within 1 meter 
+        # in the specified range.
+        # The range values given are attempting to go from -45
+        # -> 45 field of view and assume the scan values range from
+        # 0 -> 180.  
 
-	print min_scan_angle,max_scan_angle,num_scan_pts
+        min_ind = len(msg.ranges)/4
+        max_ind = 3*len(msg.ranges)/4
 
-	# Post 'avoid' twist message if object is within 1 meter 
-	# in the specified range.
-	# The range values given are attempting to go from -45
-	# -> 45 field of view and assume the scan values range from
-	# 0 -> 180.  
+        backup = 0.0
 
-	min_ind = len(msg.ranges)/4
-	max_ind = 3*len(msg.ranges)/4
+        # left side check
+#        for scan in msg.ranges[0:min_ind]:
+#            if (scan < self.detection_threshold):    
+#                backup = -1.0
+#                rospy.loginfo('Detected obstacle on left side')
+#                break
 
-	backup = 0
+        # right side check
+#        for scan in msg.ranges[max_ind:]:
+#            if (scan < self.detection_threshold):
+#                backup = -1.0
+#                rospy.loginfo('Detected obstacle on right side')
+#                break
 
-	for scan in msg.ranges[0:min_ind]:
-            if (scan < self.detection_threshold):	
-		backup = 1
-
-	for scan in msg.ranges[max_ind:]:
-            if (scan < self.detection_threshold):	
-		backup = 1
-	
         for scan in msg.ranges[min_ind:max_ind]:
 
             if (scan < self.detection_threshold):
-		self.obstacle_detected.data = True	
-        	self.obstaclePublisher.publish(self.obstacle_detected)
+                self.obstacle_detected.data = True    
+                self.obstaclePublisher.publish(self.obstacle_detected)
+                rospy.loginfo('Detected obstacle in front')
 
-		# Randomly choose a direction of angular movement
-		toss=random.randint(0,1)
-	    	self.avoidTwistMessage.linear.x = -1.0*self.backup_speed*backup
+                if not self.in_front:
+                    rospy.loginfo('Tossing the coin')
+                    self.toss = random.randint(0,1)
 
-		if toss == 0:
-	    	    self.avoidTwistMessage.angular.z = 1.0
-		else:
-	    	    self.avoidTwistMessage.angular.z = 1.0
+                self.in_front = True
+               
+                self.avoidTwistMessage.linear.x = self.backup_speed * backup
+                if -0.001 > self.avoidTwistMessage.linear.x or self.avoidTwistMessage.linear.x < 0.001:
+                    self.avoidTwistMessage.linear.x = 0.0
 
-		self.filtered_cmd_vel1.publish(self.avoidTwistMessage)
+                if self.toss == 0:
+                    self.avoidTwistMessage.angular.z = -1.0
+                else:
+                    self.avoidTwistMessage.angular.z = 1.0
 
-	        return                
+                self.filtered_cmd_vel1.publish(self.avoidTwistMessage)
+                rospy.loginfo('Published avoid Twist. linear = %0.1f, angular = %0.1f' % (self.avoidTwistMessage.linear.x, self.avoidTwistMessage.angular.z))
 
- 	self.filtered_cmd_vel1.publish(self.twistMessage)
-	
+                return                
+
+        self.in_front = False
+        self.filtered_cmd_vel1.publish(self.twistMessage)
+        rospy.loginfo('Passed original Twist')
+    
+	return
 
 if __name__ == '__main__':
 
