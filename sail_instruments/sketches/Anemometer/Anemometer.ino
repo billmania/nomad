1a895ab216fcc66c0c82c47ce1c68597b8b1d112
Bill Mania <maniabill@yahoo.com>
2015-12-07 21:28:14 -0800

    Working implementation of speed and direction in Anemometer.ino.

--- sail_instruments/sketches/Anemometer/Anemometer.ino
+++ sail_instruments/sketches/Anemometer/Anemometer.ino
@@ -9,24 +9,21 @@
  */
 
 #define MICROS_PER_MINUTE 60000000UL
+#define MICROS_PER_SECOND 1000000UL
 #define MAXIMUM_UPDATE_PERIOD 1000000UL
 
 int anemometerInterrupt = 0;
 int directionPin = 0;
 int position = 0;
-int directionVoltage, windSource;
-volatile unsigned long currentTime, previousTime, elapsedTime;
+int directionVoltage, windSource, windSpeedKnots;
+volatile unsigned long currentTime, previousTime;
+volatile double elapsedTime;
+volatile unsigned long pulses, latestPulses;
 int rpm, rpmArray[] = {0, 0, 0, 0, 0};
 
 void
-irInterrupt() {
-    currentTime = micros();
-    if (currentTime > previousTime) {
-        elapsedTime = currentTime - previousTime;
-    } else {
-        elapsedTime = 1L;
-    };
-    previousTime = currentTime;
+irInterrupt(void) {
+    pulses++;
 };
 
 void
@@ -35,9 +32,11 @@ setup()
   //Setup usb serial connection to computer
   Serial.begin(9600);
   Serial.println("Anemometer");
-  Serial.println("Version 3");
+  Serial.println("Version 5");
 
   previousTime = micros();
+  pulses = 0UL;
+  pinMode(2, INPUT_PULLUP);
   attachInterrupt(
     anemometerInterrupt,
     irInterrupt,
@@ -52,42 +51,19 @@ loop() {
      * This loop requires about 500 micros. Reading the clock
      * requires about 4 micros.
      */
-    if ((micros() - previousTime) > MAXIMUM_UPDATE_PERIOD) {
-        // The shaft is rotating too slowly, or not at all
-        Serial.println(0);
-    } else {
-        if (elapsedTime > 1) {
-		    position = (position + 1) % 5;
-		    rpmArray[position] = int(MICROS_PER_MINUTE / elapsedTime);
-		
-		    rpm = (rpmArray[0] + rpmArray[1] + rpmArray[2] +rpmArray[3] + rpmArray[4]) / 5;
-            Serial.println(rpm);
-        } else {
-            Serial.println(" Neg ");
-        };
-    };
+    latestPulses = pulses; pulses = 0UL;
+    currentTime = micros();
+    elapsedTime = currentTime - previousTime;
+    windSpeedKnots = latestPulses * (2.25 / (elapsedTime / MICROS_PER_SECOND)) * 0.868976;
+    previousTime = currentTime;
 
     directionVoltage = analogRead(directionPin);
-    if (directionVoltage > 919) {
-        windSource = 0;
-    } else if (directionVoltage > 902) {
-        windSource = 45;
-    } else if (directionVoltage > 878) {
-        windSource = 90;
-    } else if (directionVoltage > 849) {
-        windSource = 135;
-    } else if (directionVoltage > 805) {
-        windSource = 180;
-    } else if (directionVoltage > 715) {
-        windSource = 225;
-    } else if (directionVoltage > 550) {
-        windSource = 270;
-    } else {
-        windSource = 315;
-    }
+    windSource = int(0.351745 * directionVoltage - 0.054602);
 
     Serial.print("Source ");
-    Serial.println(windSource);
+    Serial.print(windSource);
+    Serial.print(" Knots ");
+    Serial.println(windSpeedKnots);
 
     delay(1000);
 }
