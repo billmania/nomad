88149a005691d48c16fcf9cedbdeb009e873df1a
Bill Mania <maniabill@yahoo.com>
2017-02-25 10:22:12 -0800

    New build and upload scripts.

--- sail_instruments/sketches/Anemometer/build.sh
+++ sail_instruments/sketches/Anemometer/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ARDUINO_DIR="/usr/local/arduino-1.6.9"
-PROJECTS_DIR="/home/bill/projects"
-PROJECT="nomad/sail_instruments/sketches/Anemometer"
+ARDUINO_DIR="/usr/local/arduino-1.6.13"
+PROJECTS_DIR="/home/bill/projects/nomad/sail_instruments/sketches"
+PROJECT="Anemometer"
 
 # -libraries "/home/bill/sketchbook/libraries" \
 # -tools "${ARDUINO_DIR}/hardware/tools/avr" \
@@ -20,15 +20,17 @@ ${ARDUINO_DIR}/arduino-builder \
  -logger=human \
  -hardware "/home/bill/.arduino15/packages" \
  -hardware "${ARDUINO_DIR}/hardware" \
+ -tools "${ARDUINO_DIR}/hardware/tools/avr" \
  -tools "/home/bill/.arduino15/packages" \
  -tools "${ARDUINO_DIR}/tools-builder" \
  -built-in-libraries "${ARDUINO_DIR}/libraries" \
+ -libraries "${ARDUINO_DIR}/Arduino/libraries" \
  -fqbn=arduino:avr:mega:cpu=atmega2560 \
- -core-api-version=10609 \
+ -core-api-version=10613 \
  -build-path "${PROJECTS_DIR}/${PROJECT}/builddir" \
  -warnings=more \
  -prefs=build.warn_data_percentage=75 \
  -verbose=true \
- "${PROJECTS_DIR}/${PROJECT}/Anemometer.ino"
+ "${PROJECTS_DIR}/${PROJECT}/${PROJECT}.ino"
 
 exit $?
--- sail_instruments/sketches/Anemometer/upload.sh
+++ sail_instruments/sketches/Anemometer/upload.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-ARDUINO_DIR="/usr/local/arduino-1.6.9"
-PROJECTS_DIR="/home/bill/projects"
-PROJECT="nomad/sail_instruments/sketches/Anemometer"
+ARDUINO_DIR="/usr/local/arduino-1.6.13"
+PROJECTS_DIR="/home/bill/projects/nomad/sail_instruments/sketches"
+PROJECT="Anemometer"
 
 cd ${PROJECTS_DIR}/${PROJECT}
 
@@ -19,6 +19,6 @@ ${ARDUINO_DIR}/arduino \
  --port /dev/ttyACM0 \
  --board arduino:avr:mega:cpu=atmega2560 \
  --pref build.path=builddir \
- ${PROJECTS_DIR}/${PROJECT}/Anemometer.ino
+ ${PROJECTS_DIR}/${PROJECT}/${PROJECT}.ino
 
 exit $?
